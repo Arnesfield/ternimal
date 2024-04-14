c33431db6b7e4be004360a27d07790244f91786a
Jefferson Rylee <rylee.jeff385@gmail.com>
2024-04-14 10:35:11 +0800

    Remove type workaround for refreshLine

--- src/lib/refresh-line.ts
+++ src/lib/refresh-line.ts
@@ -1,18 +1,14 @@
 import { Interface } from 'readline';
 import { RlInterface } from './internal.types.js';
 
-// use RlInterface for this function
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-export function refreshLine(rl: Interface, stream: NodeJS.WritableStream): void;
 export function refreshLine(
-  rl: RlInterface,
+  rl: Interface,
   stream: NodeJS.WritableStream
 ): void {
   if (!rl.terminal) {
     // do nothing
-  } else if (typeof rl._refreshLine === 'function') {
-    rl._refreshLine();
+  } else if (typeof (rl as RlInterface)._refreshLine === 'function') {
+    (rl as RlInterface)._refreshLine();
   } else {
     // NOTE: emitting resize event triggers refresh
     // see https://github.com/nodejs/node lib/internal/readline/interface.js
